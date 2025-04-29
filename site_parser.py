0078da887d0838a27ee1c354a4c8265c656ecfc2
hxllmvdx <jigoku.tyan@gmail.com>
2025-04-29 13:37:40 +0300

    fix: fixed parser to parse 16th and 26th course without mistakes. also, fixed table_header

--- site_parser.py
+++ site_parser.py
@@ -26,18 +26,29 @@ def get_last_year_programs(url='https://priem.mai.ru/base/programs/') -> tuple[s
         if i % 2 and i <= 62:
             programs.append(dict())
             programs[-1]['name'] = sibling.find('h3').text.strip()
-            programs[-1]['code'] = sibling.find('span', class_='program-codes mb-lg-0 mb-3').text.strip()
+            code = sibling.find('span', class_='program-codes mb-lg-0 mb-3').text.strip()
+            if i in [29, 51]:
+                programs[-1]['code'] = (f'{code[:len(code) // 2]} '
+                                        f'({code[len(code) // 2:]})')
+            else:
+                programs[-1]['code'] = code
             points = sibling.find('span', class_='program-points').span.text.strip().split('/')
             programs[-1]['points'] = f'{points[0]}/{points[1]}'
             if i in [35, 37, 41, 51, 57, 59]:
                 places = sibling.div.find_all('span')[8].span.text.strip().split('/')
             else:
                 places = sibling.div.find_all('span')[7].span.text.strip().split('/')
-            programs[-1]['places'] = f"{places[0]}/{places[1].replace('\n', '').replace(' ', '')}"
+            if i in [29, 51]:
+                places_paid = places[1].replace('\n', '').replace(' ', '')
+                number = places_paid.split('\xa0')[-1]
+                programs[-1]['places'] = (f'{places[0]}/{places_paid[:places_paid.rindex(number)]} '
+                                          f'({places_paid[places_paid.rindex(number):]})')
+            else:
+                programs[-1]['places'] = f"{places[0]}/{places[1].replace('\n', '').replace(' ', '')}"
             programs[-1]['subjects'] = sibling.find_all('span')[3].text.strip()
         i += 1
 
-    table_header = (f'|Код|Наименование конкурсной группы|Кол-во мест (платное/бюджет)|Баллы (платное/бюджет)|Предметы '
+    table_header = (f'|Код|Наименование конкурсной группы|Кол-во мест (бюджет / платное)|Баллы (бюджет / платное)|Предметы '
                     f'для поступления|\n'
                     f'|:-:|:----------------------------:|:--------------------------:|:--------------------:|:---------'
                     f'-------------:|\n')
