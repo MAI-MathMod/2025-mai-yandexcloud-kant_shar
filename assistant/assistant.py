f9cace227b1dd2728b18d192cfdb408552655230
hxllmvdx <jigoku.tyan@gmail.com>
2025-04-30 12:54:21 +0300

    feat: added working Function Calling to search possible courses

--- assistant/assistant.py
+++ assistant/assistant.py
@@ -12,32 +12,13 @@ config = Config(_env_file='../.env')
 sdk = YCloudML(folder_id=config.folder_id, auth=config.api_key)
 model = sdk.models.completions("yandexgpt", model_version="rc")
 
-op = sdk.search_indexes.create_deferred(
-    upload_file(sdk, '../knowledge_base/programs_table.md'),
-    index_type=HybridSearchIndexType(
-        chunking_strategy=StaticIndexChunkingStrategy(
-            max_chunk_size_tokens=1500, chunk_overlap_tokens=100
-        ),
-        combination_strategy=ReciprocalRankFusionIndexCombinationStrategy(),
-    ),
-)
-index = op.wait()
-
-search_tool = sdk.tools.search_index(index)
-table_search_tool = sdk.tools.function(SearchProgramsList)
-
-assistant = create_assistant(sdk, model, tools=[table_search_tool])
-
-thread = create_thread(sdk)
-assistant.update(instruction="""Ты сотрудник приемной комиссии Московского Авиационного Института (МАИ). 
+instruction="""Ты сотрудник приемной комиссии Московского Авиационного Института (МАИ). 
 Твоя задача консультировать абитуриентов по вопросам по поводу поступления или самого института, а также определять, 
 может ли абитуриент поступить на какое-либо направление. Когда пользователь спрашивает, на какие направления в МАИ он 
 может поступить, вызывай функцию SearchProgramsList. Посмотри на всю имеющуюся в твоем распоряжении информацию
 и сделай самый понятный и достоверный ответ. Не упоминай, что что-то можно уточнить в приемной комиссии. 
-Если что-то непонятно - переспроси""")
+Если что-то непонятно - переспроси"""
 
-thread.write('Куда я могу поступить имея 265 баллов? Я сдавал математику, информатику и русский')
-run = assistant.run(thread)
-res = run.wait()
-print(res)
-
+priem_agent = Agent(sdk=sdk, model=model, instruction=instruction, tools=[SearchProgramsList])
+print(priem_agent('Привет! Куда я могу поступить в МАИ?'))
+print(priem_agent('Я сдавал информатику, математику и русский, набрал 250 баллов. Выведи доступные мне факультеты в порядке убывания.'))
--- assistant/funcs.py
+++ assistant/funcs.py
@@ -1,5 +1,9 @@
 from pydantic import BaseModel, Field
 from typing import Optional
+import pandas as pd
+
+tb = pd.read_excel('../knowledge_base/MAI_Programs.xlsx')
+tb.columns = ['Code', 'Name', 'Budget-points', 'Paid-points', 'Exams', 'Faq', 'Courses']
 
 
 def create_thread(sdk):
@@ -24,8 +28,116 @@ class SearchProgramsList(BaseModel):
 
     name: str = Field(description='Название конкурсной группы', default=None)
     code: str = Field(description='Код специальности (три числа, разделенные точками)', default=None)
-    score: str = Field(description='Сумма баллов за экзамены', default=None)
+    score_budget: int = Field(description='Сумма баллов за экзамены, если пользователь хочет поступить на бюджет или не '
+                                          'упоминает, на какую форму обучения поступает (бюджет или платное)', default=None)
+    score_paid: int = Field(description='Сумма баллов за экзамены, если пользовательно хочет поступить на платное', default=None)
     exams: str = Field(description='Сданные экзамены (Математика, информатика и т.п.)', default=None)
     sort_order: str = Field(description='Порядок выдачи (least points, medium points, most points)', default=None)
 
     what_to_return: str = Field(description='Что вернуть (course-info или score)', default=None)
+
+    def process(self, thread):
+        return sort_table(self)
+
+
+def sort_table(req, table=tb):
+    x = table.copy()
+    if req.name:
+        x = x[x['Name'] == req.name]
+    if req.code:
+        x = x[x['Code'] == req.code]
+    if req.score_budget:
+        x = x[x['Budget-points'] <= req.score_budget]
+    if req.score_paid:
+        x = x[x['Paid-points'] <= req.score_paid]
+    if req.exams:
+        exams = req.exams.split(', ')
+        expr = [1 for exam in exams if exam.lower() in str(x['Exams']).lower()].count(1)
+        if expr >= 3:
+            pass
+        else:
+            return ('К сожалению, вы не можете поступить. '
+                    'Для участия в конкурсе необходимы результаты как минимум трех экзаменов, '
+                    'либо не нашлось подходящего вам направления.')
+    if req.sort_order and len(x) > 0:
+        if req.score_budget:
+            if req.sort_order == 'least points':
+                x = x.sort_values(by="Budget-points")
+            elif req.sort_order == 'most points':
+                x = x.sort_values(by="Budget-points", ascending=False)
+        elif req.score_paid:
+            if req.sort_order == 'least points':
+                x = x.sort_values(by='Paid-points')
+            elif req.sort_order == 'most points':
+                x = x.sort_values(by='Paid-points', ascending=False)
+        else:
+            pass
+    if x is None or len(x) == 0:
+        return 'Не найдено подходящих вам направений.'
+    return 'Нашлись такие направления:\n' + '\n'.join(f'{z["Code"]} - {z["Name"]} '
+                                                      f'({z["Budget-points"]}/{z["Paid-points"]} '
+                                                      f'бюджет/платное) с направлениями: '
+                                                      f'{"".join(z["Courses"])}' for _, z in x.head(10).iterrows())
+
+
+class Agent:
+    def __init__(self, sdk, model, assistant=None, instruction=None, search_index=None, tools=None):
+
+        self.sdk = sdk
+        self.model = model
+        self.thread = None
+
+        if assistant:
+            self.assistant = assistant
+        else:
+            if tools:
+                self.tools = {x.__name__: x for x in tools}
+                tools = [sdk.tools.function(x) for x in tools]
+            else:
+                self.tools = {}
+                tools = []
+            if search_index:
+                tools.append(sdk.tools.search_index(search_index))
+            self.assistant = create_assistant(sdk, model, tools)
+
+            if instruction:
+                self.assistant.update(instruction=instruction)
+
+    def get_thread(self, thread=None):
+        if thread is not None:
+            return thread
+        if self.thread is None:
+            self.thread = create_thread(self.sdk)
+        return self.thread
+
+    def __call__(self, message, thread=None):
+        thread = self.get_thread(thread)
+        thread.write(message)
+        run = self.assistant.run(thread)
+        res = run.wait()
+        if res.tool_calls:
+            result = []
+            for f in res.tool_calls:
+                print(
+                    f" + Вызываем функцию {f.function.name}, args={f.function.arguments}"
+                )
+                fn = self.tools[f.function.name]
+                obj = fn(**f.function.arguments)
+                x = obj.process(thread)
+                result.append({"name": f.function.name, "content": x})
+            run.submit_tool_results(result)
+            res = run.wait()
+        return res.text
+
+    def restart(self):
+        if self.thread:
+            self.thread.delete()
+            self.thread = self.sdk.threads.create(
+                name="Test", ttl_days=1, expiration_policy="static"
+            )
+
+    def done(self, delete_assistant=False):
+        if self.thread:
+            self.thread.delete()
+        if delete_assistant:
+            self.assistant.delete()
